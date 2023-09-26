ecdb73afc08a81fb21c4353f697d8de7b092d0bf
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2023-09-25 19:50:41 -0500

    added functionality

--- index.js
+++ index.js
@@ -5,7 +5,11 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  fahren = parseFloat(fahren);
+  let celc = ((fahren - 32) * 5) / 9;
+  return celc;
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +26,29 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  let message = "";
+  if (fahren < 32) {
+    message = "very cold";
+  } else if (fahren < 64) {
+    message = "cold";
+  } else if (fahren < 86) {
+    message = "warm";
+  } else message = "hot";
+
+  console.log(
+    `${fahren}° Fahrenheit converts to ${celc}° Celcius and feels ${message}`
+  );
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.trunc(Math.random() * limit) + 1;
+}
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
